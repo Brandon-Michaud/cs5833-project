82150246e517f10730a02d92298a8529a858858e
Brandon Michaud <michaudbrandond@gmail.com>
2024-05-04 15:41:37 -0500

    added solarium contract

--- src/contracts/nba_marketplace.sol
+++ src/contracts/nba_marketplace.sol
@@ -1,43 +1,47 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+// Create a marketplace for buying and selling NBA trading cards
 contract Marketplace {
+    // Defines a card
     struct Card {
-        uint id;
-        string name;
-        uint price;
-        address payable owner;
-        bool isListed;
+        uint id; // Unique identifier for card
+        string name; // Name of card
+        uint price; // Price of card in ether
+        address payable owner; // Address of owner of card
+        bool isListed; // If card is listed or not
     }
 
-    Card[] private cards;
-    mapping(uint => string) public transactionHashes;
+    Card[] private cards; // List of all cards
 
-    event CardListed(uint cardId, address owner, uint price, string transactionHash);
+    // Function to list a new card
+    function listCard(string memory _name, uint _price) public {
+        require(_price > 0, "Price must be greater than zero"); // Ensure price is valid
 
-    function listCard(string memory _name, uint _price) public returns (uint cardId) {
-        require(_price > 0, "Price must be greater than zero");
-        cardId = cards.length;
-        Card memory newCard = Card(cardId, _name, _price, payable(msg.sender), true);
-        cards.push(newCard);
-        emit CardListed(cardId, msg.sender, _price, "Initial hash or other identifier");
+        cards.push(Card(cards.length, _name, _price, payable(msg.sender), true)); // Create new card
     }
 
+    // Function to buy a card
     function buyCard(uint _cardId) public payable {
-        Card storage card = cards[_cardId];
+        Card storage card = cards[_cardId]; // Get desired card
+
+        // Check to ensure card is available, the price is satisfied, and the purchaser is not the owner
         require(card.isListed, "Card must be listed for sale");
         require(msg.value >= card.price, "Value sent is not enough");
         require(card.owner != msg.sender, "Owner cannot buy their own card");
 
+        // Pay owner and transfer ownership
         card.owner.transfer(msg.value);
         card.owner = payable(msg.sender);
         card.isListed = false;
     }
 
+    // Function to get a single card given the ID
     function getCard(uint _cardId) public view returns (Card memory) {
         return cards[_cardId];
     }
 
+    // Function to get the total number of cards
     function getCardCount() public view returns (uint) {
         return cards.length;
     }
